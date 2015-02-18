71a2cb2584e61163adcdaef261aec69446a63565
Fenyvesi Tamás <a.fenyvesi.tamas@gmail.com>
2015-02-18 22:04:15 +0100

    done with the negs error throw

--- stringcalc/src/main/java/com/mycompany/stringcalc/StringCalculator.java
+++ stringcalc/src/main/java/com/mycompany/stringcalc/StringCalculator.java
@@ -5,6 +5,8 @@
  */
 package com.mycompany.stringcalc;
 
+import java.util.regex.Pattern;
+
 /**
  *
  * @author poker07
@@ -13,28 +15,40 @@ public class StringCalculator {
 
     /**
      * Adding the numbers in the given string can be separated with any char or
-     * string.     
-     * if no numbers in the string 0 will be given back
+     * string. if no numbers in the string 0 will be given back
+     *
      * @param src - the string with numbers in it
      * @return sum of numbers in the string
      */
-    public int add(String src) {
+    public int add(String src) throws NumberFormatException{
+        String num = "0123456789";
+
         if (src.isEmpty()) {
             return 0;
         }
 
-        String[] numbers = src.split("[^0123456789]");
-        int sum = 0;
+        String[] numbers = src.split("[^-" + num + "]");
+        String negs = "";
+        int sum = 0, i;
+
         for (String number : numbers) {
             try {
+                i = Integer.valueOf(number);
+                if (i < 0) {
+                    negs += String.valueOf(i) + ", ";
+                }
                 sum += Integer.valueOf(number);
             } catch (NumberFormatException e) {
+                System.out.println("number: " + number + " !! errr");
                 continue;
             }
         }
 
-        return sum;
+        if (!negs.equals("")) {
+            throw new NumberFormatException(negs);
+        }
 
+        return sum;
     }
 
 }
